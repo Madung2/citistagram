7bdcdfe3b66cacfab14d7a4bc047b790274c2e51
Madung2 <104334219+Madung2@users.noreply.github.com>
2022-05-02 15:53:09 +0900

    margin등 요소 위치 정리 1차 완성

--- app.py
+++ app.py
@@ -10,6 +10,13 @@ db = client.dbsparta
 @app.route('/')
 def home():
    return render_template('index.html')
+
+@app.route('/profile_page')
+def profile():
+   return render_template('profile_page.html')
+
+
+
 ####################첫번째 코멘트창########################################
 @app.route("/insta_comment", methods=["POST"])
 def insta_comment_post():
@@ -58,11 +65,29 @@ def insta_comment_get3():
     all_comment = list(db.insta_comment3.find({}, {'_id': False}))
     return jsonify({'comments':all_comment})
 
+####################네번째 코멘트창########################################
+@app.route("/insta_comment4", methods=["POST"])
+def insta_comment_post4():
+
+    comment_receive = request.form['comment_give']
+
+    doc = {'comment':comment_receive}
+    db.insta_comment4.insert_one(doc)
+    return jsonify({'msg':'소중한 댓글 감사합니다'})
 
 
+@app.route("/insta_comment4", methods=["GET"])
+def insta_comment_get4():
+    all_comment = list(db.insta_comment4.find({}, {'_id': False}))
+    return jsonify({'comments':all_comment})
 
-
-
+###################좋아요 갯수 카트########################################
+@app.route("/like_number1", methods=["POST"])
+def post_like_number():
+    like_receive = request.form['like_give1']
+    doc = {'likes1':like_receive}
+    db.like_number1.insert_one(doc)
+    return jsonify({'msg':'좋아요 감사합니다.'})
 
 
 
